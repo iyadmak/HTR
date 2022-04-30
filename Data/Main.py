2712c3307cbfefa19c76bca82f275a35009eff22
moha-boukhatem <moha94orly@gmail.com>
2022-04-30 19:16:02 +0100

    modify README file

--- Data/Main.py
+++ Data/Main.py
@@ -4,14 +4,14 @@ from InputData import ImagePathsLabels
 
 
 words_list = CleanData()
-print("✅  Clean all errored entries ... ")
+print("\n ✅  Clean all errored entries ... ")
 
 train_samples ,validation_samples ,test_samples = SplitData(words_list)
 
-print("✅  Split the dataset into three subsets with a 90:5:5 ...")
+print(" ✅  Split the dataset into three subsets with a 90:5:5 ...")
 
 train_img_paths, train_labels = ImagePathsLabels(train_samples)
 validation_img_paths, validation_labels = ImagePathsLabels(validation_samples)
 test_img_paths, test_labels = ImagePathsLabels(test_samples)
 
-print("✅  Generate lists of paths & image names ...")
+print(" ✅  Generate lists of paths & image names ...\n")
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Handwritten Text Recognition system
+
+
+# Handwritten Text Recognition with TensorFlow
+
+* **Graduation project for my bachelor's degree**
+
+
+Handwritten Text Recognition (HTR) system based on LSTMs & implemented with TensorFlow (TF) and trained on the IAM off-line HTR dataset.
+The model takes **images of single words or text lines (multiple words) as input** and **outputs the recognized text**.
+
