9511126e16989a053c12e333e5e25ad4f13ee2a0
moha-boukhatem <moha94orly@gmail.com>
2022-05-04 14:32:59 +0100

    edit PrintFunctions file

--- Data/Main.py
+++ Data/Main.py
@@ -7,7 +7,6 @@ from PrintFunctions import *
 MainFunc()
 
 ##### CleanData
-words_list,words = CleanData()
 CleanDataFunc()
 
 
@@ -16,7 +15,4 @@ SplitDataFunc()
 
 
 ##### ImagePathsLabels
-train_img_paths, train_labels = ImagePathsLabels(train_samples)
-validation_img_paths, validation_labels = ImagePathsLabels(validation_samples)
-test_img_paths, test_labels = ImagePathsLabels(test_samples)
 ImagePathsLabelsFunc()
--- Data/PrintFunctions.py
+++ Data/PrintFunctions.py
@@ -2,8 +2,18 @@ from CleanData import CleanData
 from SplitData import SplitData
 from InputData import ImagePathsLabels
 
+##### FunctionsCalled()
+
 words_list,words = CleanData()
+
 train_samples ,validation_samples ,test_samples = SplitData(words_list)
+
+train_img_paths, train_labels = ImagePathsLabels(train_samples)
+validation_img_paths, validation_labels = ImagePathsLabels(validation_samples)
+test_img_paths, test_labels = ImagePathsLabels(test_samples)
+
+
+##### PrintFunctions()
 
 def MainFunc():
     print("\n ###### Preprocess Data ###### \n")
