43b0a53d6b75c828dbdba963a99a60b80d6f30f8
moha-boukhatem <moha94orly@gmail.com>
2022-05-04 12:56:30 +0100

    add PrintFunctions file

---
+++ Data/PrintFunctions.py
@@ -0,0 +1,8 @@
+def CleanDataFunc():
+    print("\n ✅  Clean all errored entries ... ")
+
+def SplitDataFunc():
+    print(" ✅  Split the dataset into three subsets with a 90:5:5 ...")
+
+def ImagePathsLabelsFunc():
+    print(" ✅  Generate lists of paths & image names ...\n")
