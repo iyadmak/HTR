9648228879c394872195a949434b80119efb5943
moha-boukhatem <moha94orly@gmail.com>
2022-05-04 12:57:35 +0100

    add PrintFunctions file

--- Data/Main.py
+++ Data/Main.py
@@ -1,17 +1,21 @@
 from CleanData import CleanData
 from SplitData import SplitData
 from InputData import ImagePathsLabels
+from PrintFunctions import *
 
 
+##### CleanData
 words_list = CleanData()
-print("\n ✅  Clean all errored entries ... ")
+CleanDataFunc()
 
+
+##### SplitData
 train_samples ,validation_samples ,test_samples = SplitData(words_list)
+SplitDataFunc()
 
-print(" ✅  Split the dataset into three subsets with a 90:5:5 ...")
 
+##### ImagePathsLabels
 train_img_paths, train_labels = ImagePathsLabels(train_samples)
 validation_img_paths, validation_labels = ImagePathsLabels(validation_samples)
 test_img_paths, test_labels = ImagePathsLabels(test_samples)
-
-print(" ✅  Generate lists of paths & image names ...\n")
+ImagePathsLabelsFunc()
